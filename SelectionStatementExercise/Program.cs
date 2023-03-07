23d849bb62da2c0d8bf2294a62c19fc305a983a4
Alec Hancock <alec.m.hancock@gmail.com>
2023-03-06 21:30:39 -0600

    Assignment completed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            //var favNumber = r.Next(1, 100);
+            var favNumber = 75;
+            
+
+            Console.WriteLine("Pick a number between 1-1000");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low.");
+
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high.");
+
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+
+            }
+
         }
     }
 }
